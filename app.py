df2b46cd39d18c282b025e26fcd34b2734e3cb39
JulianChan123 <a20216390@alumnos.uady.mx>
2023-11-20 11:49:36 -0600

    all test succesfull

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, json
 
 app = Flask(__name__)
 
@@ -20,52 +20,59 @@ def getAlumno(id):
 @app.route('/alumnos', methods=['POST'])
 def addAlumno():
     try:
-        new_id = int(request.json['id'])
-        if any(alumno['id'] == new_id for alumno in alumnos):
-            return jsonify({"error": "El ID ya está en uso"}), 400
+        id = int(request.json['id'])
+        nombres = request.json['nombres']
+        apellidos = request.json['apellidos']
+        matricula = request.json['matricula']
+        promedio = int(request.json['promedio'])
+
+        # Validar campos no nulos
+        if None in [nombres, apellidos, matricula]:
+            return jsonify({"error": "Los campos de nombres, apellidos y matricula no pueden ser nulos"}), 400
+
+        # Validar promedio no negativo
+        if promedio < 0:
+            return jsonify({"error": "El promedio no puede ser negativo"}), 400
 
         new_alumno = {
-            "id": int(request.json['id']),
-            "nombres": str(request.json['nombres']),
-            "apellidos": str(request.json['apellidos']),
-            "matricula": str(request.json['matricula']),
-            "promedio": int(request.json['promedio'])
+            "id": id,
+            "nombres": nombres,
+            "apellidos": apellidos,
+            "matricula": matricula,
+            "promedio": promedio
         }
         alumnos.append(new_alumno)
         return jsonify({"message": "Alumno añadido correctamente", "alumnos": alumnos}), 201
-    # Excepción id y promedio deben ser int
-    except ValueError:    
+    except ValueError:
         return jsonify({"error": "El ID y el promedio deben ser números enteros"}), 400
-    # Excepcion por si falta un campo
     except KeyError as e:
         return jsonify({"error": f"Campo faltante: {str(e)}"}), 400
-    #Excepcion para Internal Server Error
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+
 
 @app.route('/alumnos/<int:id>', methods=['PUT'])
 def updateAlumno(id):
     try:
-        new_id = int(request.json['id'])
-
-        #Verificar que el id no este en uso
-        if any(alumno['id'] == new_id for alumno in alumnos):
-            return jsonify({"error": "El ID ya está en uso"}), 400
-
-        alumno = next((alumno for alumno in alumnos if alumno['id'] == id), None)
-
-        if alumno:
+        
+        # Validar campos no nulos
+        nombres = request.json['nombres']
+        apellidos = request.json['apellidos']
+        matricula = request.json['matricula']
+        if None in [nombres, apellidos, matricula]:
+            return jsonify({"error": "Los campos de nombres, apellidos y matricula no pueden ser nulos"}), 400
+        
+        alumnoFound = next((alumno for alumno in alumnos if alumno['id'] == id), None)
+        if alumnoFound:
             if 'id' in request.json:
-                alumno['id'] = int(request.json['id'])
+                alumnoFound['id'] = int(request.json['id'])
             if 'nombres' in request.json:
-                alumno['nombres'] = str(request.json['nombres'])
+                alumnoFound['nombres'] = str(request.json['nombres'])
             if 'apellidos' in request.json:
-                alumno['apellidos'] = str(request.json['apellidos'])
+                alumnoFound['apellidos'] = str(request.json['apellidos'])
             if 'matricula' in request.json:
-                alumno['matricula'] = str(request.json['matricula'])
+                alumnoFound['matricula'] = str(request.json['matricula'])
             if 'promedio' in request.json:
-                alumno['promedio'] = int(request.json['promedio'])
-            return jsonify({"message": "Alumno modificado correctamente", "alumno": alumno})
+                alumnoFound['promedio'] = int(request.json['promedio'])
+            return jsonify({"message": "Alumno modificado correctamente", "alumno": alumnoFound})
         else:
             return jsonify({"error": "Alumno no encontrado"}), 404
     
@@ -75,13 +82,9 @@ def updateAlumno(id):
     #Excepcion por si falta algún campo
     except KeyError as e:
         return jsonify({"error": f"Campo faltante: {str(e)}"}), 400
-    #Excepcion para Internal Server Error
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
 
 @app.route('/alumnos/<int:id>', methods=['DELETE'])
 def deleteAlumno(id):
-    try:
         alumno = next((alumno for alumno in alumnos if alumno['id'] == id), None)
         
         if alumno:
@@ -89,10 +92,6 @@ def deleteAlumno(id):
             return jsonify({"message": "Alumno eliminado correctamente", "alumno_eliminado": alumno})
         else:
             return jsonify({"error": "Alumno no encontrado"}), 404
-    
-    #Excepcion para Internal Server Error
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
 
 @app.route('/profesores')
 def getProfesores():
@@ -104,84 +103,90 @@ def getProfesor(id):
         if profesorFound:
             return jsonify(profesorFound)
         else:
-            return jsonify({'error': 'Profesor no encontrado'}), 404
+            return jsonify({'error': 'Alumno no encontrado'}), 404
 
 @app.route('/profesores', methods=['POST'])
 def addProfesor():
     try:
-        new_id = int(request.json['id'])
-        if any(profesor['id'] == new_id for profesor in profesores):
-            return jsonify({"error": "El ID ya está en uso"}), 400
+        id = int(request.json['id'])
+        nombres = str(request.json['nombres'])
+        apellidos = str(request.json['apellidos'])
+        numeroEmpleado = str(request.json['numeroEmpleado'])
+        horasClase = int(request.json['horasClase'])
+
+        # Validar campos no nulos
+        if None in [nombres, apellidos, numeroEmpleado]:
+            return jsonify({"error": "Los campos de nombres, apellidos y numero de empleado no pueden ser nulos"}), 400
+
+        # Validar promedio no negativo
+        if horasClase < 0:
+            return jsonify({"error": "las horas de clase no puede ser negativo"}), 400
 
         new_profesor = {
-            "id": int(request.json['id']),
-            "nombres": str(request.json['nombres']),
-            "apellidos": str(request.json['apellidos']),
-            "numeroEmpleado": str(request.json['numeroEmpleado']),
-            "horasClase": int(request.json['horasClase'])
+            "id": id,
+            "nombres": nombres,
+            "apellidos": apellidos,
+            "numeroEmpleado": numeroEmpleado,
+            "horasClase": horasClase
         }
         profesores.append(new_profesor)
-        return jsonify({"message": "Profesor añadido correctamente", "profesores": profesores})
-    # Excepción id y promedio deben ser int
-    except ValueError:    
-        return jsonify({"error": "El ID y las horas de clase deben ser números enteros"}), 400
-    # Excepcion por si falta un campo
+        return jsonify({"message": "Alumno añadido correctamente", "alumnos": profesores}), 201
+    except ValueError:
+        return jsonify({"error": "El id y las horas de clase deben ser números enteros"}), 400
     except KeyError as e:
         return jsonify({"error": f"Campo faltante: {str(e)}"}), 400
-    #Excepcion para Internal Server Error
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-        
+
 @app.route('/profesores/<int:id>', methods=['PUT'])
-def updateProfesor(id):
+def updateProfesores(id):
     try:
-        new_id = int(request.json['id'])
 
-        #Verificar que el id no este en uso
-        if any(profesor['id'] == new_id for profesor in profesores):
-            return jsonify({"error": "El ID ya está en uso"}), 400
+        nombres = str(request.json['nombres'])
+        apellidos = str(request.json['apellidos'])
+        numeroEmpleado = str(request.json['numeroEmpleado'])
+        horasClase = int(request.json['horasClase'])
+        
+        # Validar campos no nulos
+        if None in [nombres, apellidos, numeroEmpleado]:
+            return jsonify({"error": "Los campos de nombres, apellidos y numero de empleado no pueden ser nulos"}), 400
 
-        profesor = next((profesor for profesor in profesores if profesor['id'] == id), None)
+        # Validar promedio no negativo
+        if horasClase < 0:
+            return jsonify({"error": "las horas de clase no puede ser negativo"}), 400
 
-        if profesor:
+
+        profesorFound = next((profesor for profesor in profesores if profesor['id'] == id), None)
+        if profesorFound:
             if 'id' in request.json:
-                profesor['id'] = int(request.json['id'])
+                profesorFound['id'] = int(request.json['id'])
             if 'nombres' in request.json:
-                profesor['nombres'] = str(request.json['nombres'])
+                profesorFound['nombres'] = str(request.json['nombres'])
             if 'apellidos' in request.json:
-                profesor['apellidos'] = str(request.json['apellidos'])
+                profesorFound['apellidos'] = str(request.json['apellidos'])
             if 'numeroEmpleado' in request.json:
-                profesor['numeroEmpleado'] = str(request.json['numeroEmpleado'])
+                profesorFound['numeroEmpleado'] = str(request.json['numeroEmpleado'])
             if 'horasClase' in request.json:
-                profesor['horasClase'] = int(request.json['horasClase'])
-            return jsonify({"message": "Profesor modificado correctamente", "alumno": profesor})
+                profesorFound['horasClase'] = int(request.json['horasClase'])
+            return jsonify({"message": "profesor modificado correctamente", "profesor": profesorFound})
         else:
-            return jsonify({"error": "Profesor no encontrado"}), 404
+            return jsonify({"error": "profesor no encontrado"}), 404
     
     #Excepcion para que el id y el promedio sean int
     except ValueError:
-        return jsonify({"error": "El id y las horas de clase debe ser un número entero"}), 400
+        return jsonify({"error": "El id y el horas de clase deben ser un número entero"}), 400
     #Excepcion por si falta algún campo
     except KeyError as e:
         return jsonify({"error": f"Campo faltante: {str(e)}"}), 400
-    #Excepcion para Internal Server Error
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
 
 @app.route('/profesores/<int:id>', methods=['DELETE'])
 def deleteProfesor(id):
-    try:
         profesor = next((profesor for profesor in profesores if profesor['id'] == id), None)
         
         if profesor:
             profesores.remove(profesor)
-            return jsonify({"message": "Profesor eliminado correctamente", "Profesor eliminado": profesor})
+            return jsonify({"message": "profesor eliminado correctamente", "profesor_eliminado": profesor})
         else:
-            return jsonify({"error": "Profesor no encontrado"}), 404
+            return jsonify({"error": "profesor no encontrado"}), 404
     
-    #Excepcion para Internal Server Error
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
